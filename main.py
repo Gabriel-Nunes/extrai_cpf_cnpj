1ae4b947d86e574a22a6beca7cd8991a630ad256
Gabriel-Nunes <31461719+Gabriel-Nunes@users.noreply.github.com>
2021-02-06 00:02:27 -0300

    adicionadas funções de extração de cpf/cnpj

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 # -*- coding: utf-8 -*-
 from modules.utils import select_files
+from winmagic import magic
+import os
 
 
-class File:
-    def __init__(self, files_paths):
-        self.files_paths = files_paths
+class Doc:
+    def __init__(self, file_path):
+        self._type = ''
+        self.path = file_path
+        self.filename = os.path.basename(file_path)
+        self._text = ''
+
+    @property
+    def type(self):
+        mime = magic.Magic(mime=True)
+        self._type = mime.from_file(self.path)
+        return self._type
+
+    # TODO get the file content
+    @property
+    def text(self):
+        return
+
+    # TODO find cpfs on text
+
+    # TODO find cnpjs on text
+
+
+if __name__ == '__main__':
+    doc_teste = Doc('test\\test.pdf')
+
