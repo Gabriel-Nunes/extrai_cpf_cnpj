41e0fae4b7ab047f2688902647fe239588710de3
Gabriel-Nunes <31461719+Gabriel-Nunes@users.noreply.github.com>
2021-02-09 22:18:52 -0300

    requirements update

--- extrai_cpf_cnpj.py
+++ extrai_cpf_cnpj.py
@@ -12,70 +12,78 @@ class Doc:
         self.type = file_type
         self.path = file_path
         self.filename = os.path.basename(file_path)
-        self._text = ''
+        self.text = ''
 
-    # @property
-    # def type(self):
-    #     mime = magic.Magic(mime=True)
-    #     self._type = mime.from_file(self.path)
-    #     return self._type
-
-    # TODO get the file content
+    # Get the file content
     def get_text(self):
         if self.type == '*.pdf':
             try:
-                text = extract_text(self.path)
-                return text
+                self.text = extract_text(self.path)
             except TypeError:
                 raise f"Erro ao ler o arquivo {0}. É um 'pdf' pesquisável?".format(self.filename)
         elif self.type == '*.docx':
             try:
-                text = docx2txt.process(self.path)
-                return text
+                self.text = docx2txt.process(self.path)
             except TypeError:
                 raise f"Erro ao ler o arquivo {0}. É um '.docx'?".format(self.filename)
 
-    # TODO find cpfs on text
+    # Find CPFs on text
     def get_cpfs(self):
         cpfs = procura_cpf(self.text)
         return [(cpf, self.filename) for cpf in cpfs]
 
-    # TODO find cnpjs on text
+    # Find cnpjs on text
     def get_cnpjs(self):
         cpfs = procura_cpf(self.text)
         return [(cpf, self.filename) for cpf in cpfs]
 
 
 if __name__ == '__main__':
+    # Get the type of the files from user according to its extensions
     file_type = {'1': '*.pdf', '2': '*.docx'}
     input_type = choose_type()
+
+    # Check the input [3 - leave program]
     if input_type == '3':
         sys.exit()
+    # If the input option is not valid, ask again
     elif input_type not in ['1', '2', '3']:
         choose_type()
+
+    # Set the working directory to the current
     BASE_PATH = os.getcwd()
+
+    # Lists to store final results
     cpf_results = []
     cnpj_results = []
 
+    # Open GUI window to user select the sources he wants to get CPFs/CNPJs
     files = select_files(file_type[input_type])
+
     print('\nLendo arquivos...\n')
-    for file in tqdm(files):
-        doc = Doc(file, file_type[input_type])
-        text = doc.get_text()
-        cpfs = procura_cpf(text)
-        cnpjs = procura_cnpj(text)
+    for file in tqdm(files):  # To each file selected
+        doc = Doc(file, file_type[input_type])  # Instantiate a Doc object
+
+        # Store the file's text
+        # text = doc.get_text()
+        doc.get_text()
+        # Get file's valid CPFs/CNPJs
+        cpfs = procura_cpf(doc.text)
+        cnpjs = procura_cnpj(doc.text)
+
+        # Save each CPF/CNPJ as a string (Ex: "81781726255;source_file.docx")
         for cpf in cpfs:
             cpf_results.append(';'.join([cpf, doc.filename]))
         for cnpj in cnpjs:
             cnpj_results.append(';'.join([cnpj, doc.filename]))
 
-    # Create a .csv to store cpfs.
+    # Create a .csv to store CPFs.
     print('\nGravando CPFs...')
     with open("cpfs_encontrados.csv", mode="a", newline='\n') as new_file:
         for result in tqdm(cpf_results):
             new_file.write(f'{result}\n')
 
-    # Create a .csv to store cnpjs.
+    # Create a .csv to store CNPJs.
     print('\nGravando CNPJs...')
     with open("cnpjs_encontrados.csv", mode="a", newline='\n') as new_file:
         for result in tqdm(cnpj_results):
